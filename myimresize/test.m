bfd614dba6931d4e7928470e415b740bb0210205
double Z <doubleZ0108@163.com>
2019-09-16 18:04:55 +0800

    add Time Effcient Comparison test

--- myimresize/test.m
+++ myimresize/test.m
@@ -1,7 +1,7 @@
 clear
 clc
 
-img = imread('Resources/color2.jpg');
+img = imread('Resources/color1.jpg');
 figure;
 imshow(img, []);
 
@@ -9,6 +9,11 @@ small_img = myimresize(img, 0.5, 'bicubic');
 figure;
 imshow(small_img,[]);      % 第二个参数 -> 自动调整数据的范围以便于显示；如果不加第二个参数则矩阵是正确的，但是图像是全白的
 
-big_img = myimresize(img, 2, 'bicubic');
+
+mid_img = myimresize(img, 1, 'nearest');
+figure;
+imshow(mid_img, []);
+
+big_img = myimresize(img, 2, 'bilinear');
 figure;
 imshow(big_img, []);
