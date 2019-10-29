30bbe061ab8373b66ff3d9f589bc1a3bd87b92cf
double Z <doubleZ0108@163.com>
2019-10-29 16:01:48 +0800

    :alembic:add comment to Loacl Adapt Histeq

--- NightTime Road Contrast Enhancement/src/LocalAdaptHisteq.m
+++ NightTime Road Contrast Enhancement/src/LocalAdaptHisteq.m
@@ -1,7 +1,7 @@
 clear;
 clc;
 
-img_path = 'Resources/';
+img_path = '../Resources/';
 imgs_name = ['hw1_dark_road_1.jpg';'hw1_dark_road_2.jpg';'hw1_dark_road_3.jpg'];
 result_path = 'ScreenShots/Local Adapt Histogram Equalization/';
 [M,N] = size(imgs_name);
@@ -10,8 +10,10 @@ for k=1:M
     img_name = imgs_name(k,:);
     I = imread([img_path, img_name]);
     
+    % adaptive histogram equalization
     numtiles = 16; cliplimit = 0.018;
-    J = adapthisteq(I, 'NumTiles',[numtiles, numtiles], 'ClipLimit', cliplimit);
+    J = adapthisteq(I, ...
+                    'NumTiles',[numtiles, numtiles], ...
+                    'ClipLimit', cliplimit);
     imwrite(J, [result_path, 'adapthisteq_16_16_0.018_', num2str(k), '.png']);
-    
 end
